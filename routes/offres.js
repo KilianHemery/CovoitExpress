9507f1f744cc53ec0dd87906b7ead6465e0a60b1
KilianHemery <kilian85130@hotmail.fr>
2017-09-26 14:54:31 +0200

    Gestion des offres : définition des routes

--- routes/offres.js
+++ routes/offres.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 var offreController = require('../controllers/offreController');
 
-/* GET home page. */
+//GET all offres
 router.get('/',offreController.offre_list);
+
+//GET specific offre
+router.get('/:id',offreController.offre_detail);
+
+//POST one Offre
+router.post('/',offreController.offre_ajout_post);
 
 module.exports = router;
